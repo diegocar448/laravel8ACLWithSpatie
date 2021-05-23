6f2826b3c3e5e30782fa1944212360f15adbedba
Diego Cardoso <diegocar448@hotmail.com>
2021-05-23 17:28:02 -0300

    Configuração Inertia part1

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Inertia\Inertia;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -23,6 +24,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        $version = "1.0";
+        Inertia::version($version);
     }
 }
---
+++ resources/js/Pages/Home.vue
@@ -0,0 +1,13 @@
+<template lang="pug">
+    <div>
+        Sou Sweet Home
+    </div>
+
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
